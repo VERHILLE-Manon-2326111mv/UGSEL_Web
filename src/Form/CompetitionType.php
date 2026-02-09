d85f0f9f6a5abb52d6cab38be5f4c741f56e0698
VERNAGUT-Titouan-2326112-A <titouan.vernagut@etu.univ-amu.fr>
2026-02-09 16:53:54 +0100

    add CRUD for all the entities

---
+++ src/Form/CompetitionType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Championship;
+use App\Entity\Competition;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CompetitionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('championship', EntityType::class, [
+                'class' => Championship::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Competition::class,
+        ]);
+    }
+}
