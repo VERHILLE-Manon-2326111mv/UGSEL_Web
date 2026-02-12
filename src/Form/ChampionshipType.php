630b71348d2d02aa74c482d1367adbfea2867c32
ronan <ronan.garrigues@etu.univ-amu.fr>
2026-02-12 15:24:44 +0100

    add complete CRUD for all the entities

--- src/Form/ChampionshipType.php
+++ src/Form/ChampionshipType.php
@@ -3,10 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Championship;
-use App\Entity\Sport;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,18 +12,8 @@ class ChampionshipType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name', TextType::class, [
-                'label' => 'Nom du championnat'
-            ])
-            ->add('type', TextType::class, [
-                'label' => 'Type'
-            ])
-            ->add('sport', EntityType::class, [
-                'class' => Sport::class,
-                'choice_label' => 'name',
-                'label' => 'Sport associé',
-                'placeholder' => 'Choisir un sport...',
-            ])
+            ->add('name')
+            ->add('type')
         ;
     }
 
