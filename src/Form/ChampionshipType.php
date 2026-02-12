6a4327823ad9c726ab387be28c12a8a49360c0ef
ronan <ronan.garrigues@etu.univ-amu.fr>
2026-02-12 15:13:37 +0100

    add complete CRUD for all the entities

--- src/Form/ChampionshipType.php
+++ src/Form/ChampionshipType.php
@@ -3,7 +3,10 @@
 namespace App\Form;
 
 use App\Entity\Championship;
+use App\Entity\Sport;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +15,18 @@ class ChampionshipType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('type')
+            ->add('name', TextType::class, [
+                'label' => 'Nom du championnat'
+            ])
+            ->add('type', TextType::class, [
+                'label' => 'Type'
+            ])
+            ->add('sport', EntityType::class, [
+                'class' => Sport::class,
+                'choice_label' => 'name',
+                'label' => 'Sport associé',
+                'placeholder' => 'Choisir un sport...',
+            ])
         ;
     }
 
--- src/Form/CompetitionType.php
+++ src/Form/CompetitionType.php
@@ -6,6 +6,7 @@ use App\Entity\Championship;
 use App\Entity\Competition;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType; // Import ajouté
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,10 +15,14 @@ class CompetitionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
+            ->add('name', TextType::class, [
+                'label' => 'Nom de la compétition'
+            ])
             ->add('championship', EntityType::class, [
                 'class' => Championship::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name',
+                'label' => 'Championnat associé',
+                'placeholder' => 'Choisir un championnat',
             ])
         ;
     }
